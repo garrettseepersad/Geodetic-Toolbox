7ed8ce029450269bf4aad4479e7275b10709e43b
G <GarrettSeepersad@gmail.com>
2018-06-17 16:57:01 +0200

    doy2jd()

---
+++ doy2jd.py
@@ -0,0 +1,18 @@
+from cal2jd import cal2jd
+
+def doy2jd(yr,doy):
+#% DOY2JD  Converts year and day of year to Julian date.
+#% . Non-vectorized version. See also CAL2JD, GPS2JD,
+#%   JD2CAL, JD2DOW, JD2DOY, JD2GPS, JD2YR, YR2JD.
+#% Version: 24 Apr 99
+#% Usage:   jd=doy2jd(yr,doy)
+#% Input:    yr - year
+#%          doy - day of year
+#% Output:  jd  - Julian date
+#
+#% Copyright (c) 2011, Michael R. Craymer
+#% All rights reserved.
+#% Email: mike@craymer.com
+
+    jd = cal2jd(yr,1,0) + doy;
+    return (jd)
