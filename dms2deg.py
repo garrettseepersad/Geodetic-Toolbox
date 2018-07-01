3cec4d1891d9cd0a92c3ebeea6876962378790d7
G <GarrettSeepersad@gmail.com>
2018-07-01 15:49:22 +0200

    dms2deg

---
+++ dms2deg.py
@@ -0,0 +1,26 @@
+import math
+
+def dms2deg(dms):
+#% DMS2DEG  Converts degrees-minutes-seconds to decimal degrees.
+#%   Vectorized.
+#% Version: 12 Mar 00
+#% Useage:  deg=dms2deg(dms)
+#% Input:   dms - [d m s] array of angles in deg-min-sec, where
+#%                d = vector of degrees
+#%                m = vector of minutes
+#%                s = vector of seconds
+#% Output:  deg - vector of angles in decimal degrees
+
+#% Copyright (c) 2011, Michael R. Craymer
+#% All rights reserved.
+#% Email: mike@craymer.com
+
+    d=dms[0];
+    m=dms[1];
+    s=dms[2];
+    deg=math.fabs(d)+math.fabs(m)/60+math.fabs(s)/3600;
+    
+    if (d<0 or m<0 or s<0):
+        deg=-deg;
+
+    return(deg)
