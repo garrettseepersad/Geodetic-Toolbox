09478b5189ba2b371ab01086f8a1a379147f3faf
G <GarrettSeepersad@gmail.com>
2018-07-01 18:54:20 +0200

    jd2dow

---
+++ jd2dow.py
@@ -0,0 +1,27 @@
+import math
+import numpy
+
+def jd2dow(jd):
+#% JD2DOW  Converts Julian date to day of week (Sun=1,...,Sat=7)
+#%   Adapted from the algorithm from "Practical Astronomy with
+#%   Your Calculator" (Third Edition) by Peter Duffett-Smith,
+#%   Cambridge Univeristy Press, 1988. Non-vectorized version.
+#%   See also CAL2JD, DOY2JD, GPS2JD, JD2CAL, JD2DOY, JD2GPS,
+#%   JD2YR, YR2JD.
+#% Version: 24 Apr 99
+#% Usage:   dow=jd2dow(jd)
+#% Input:   jd  - Julian date
+#% Output:  dow - day of week (1=Sun,...,7=Sat)
+
+#% Copyright (c) 2011, Michael R. Craymer
+#% All rights reserved.
+#% Email: mike@craymer.com
+
+    if (jd < 0):
+      print('Julian date must be greater than or equal to zero');
+      return;
+
+    dow = math.floor(numpy.remainder(jd+1.5,7))+1;
+
+    return(dow)
+
